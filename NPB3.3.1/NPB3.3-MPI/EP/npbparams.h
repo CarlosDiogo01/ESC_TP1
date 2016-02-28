04f776c0c4f7ae85d16dfd8f9a7702c7e5537e05
CarlosDiogo01 <a59905@search6.local>
2016-02-28 00:29:50 +0000

    MPI scripts set to submit jobs and achieve the results

--- NPB3.3.1/NPB3.3-MPI/EP/npbparams.h
+++ NPB3.3.1/NPB3.3-MPI/EP/npbparams.h
@@ -1,4 +1,4 @@
-c NPROCS = 1 CLASS = A
+c NPROCS = 32 CLASS = C
 c  
 c  
 c  This file is generated automatically by the setparams utility.
@@ -6,26 +6,26 @@ c  It sets the number of processors and the class of the NPB
 c  in this directory. Do not modify it by hand.
 c  
         character class
-        parameter (class ='A')
+        parameter (class ='C')
         integer m, npm
-        parameter (m=28, npm=1)
+        parameter (m=32, npm=32)
         logical  convertdouble
         parameter (convertdouble = .false.)
         character*11 compiletime
-        parameter (compiletime='23 Feb 2016')
+        parameter (compiletime='28 Feb 2016')
         character*5 npbversion
         parameter (npbversion='3.3.1')
-        character*7 cs1
-        parameter (cs1='mpifort')
+        character*6 cs1
+        parameter (cs1='mpif77')
         character*9 cs2
         parameter (cs2='$(MPIF77)')
-        character*22 cs3
-        parameter (cs3='-L/usr/local/lib -lmpi')
-        character*20 cs4
-        parameter (cs4='-I/usr/local/include')
-        character*2 cs5
-        parameter (cs5='-O')
-        character*2 cs6
-        parameter (cs6='-O')
+        character*17 cs3
+        parameter (cs3='-lmpi -lgfortran ')
+        character*6 cs4
+        parameter (cs4='(none)')
+        character*14 cs5
+        parameter (cs5='-O3 -lgfortran')
+        character*15 cs6
+        parameter (cs6='-O3 -lgfortran ')
         character*6 cs7
         parameter (cs7='randi8')
