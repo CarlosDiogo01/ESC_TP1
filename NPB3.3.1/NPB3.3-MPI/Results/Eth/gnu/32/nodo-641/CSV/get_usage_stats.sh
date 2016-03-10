827da53e3b13e67664eacb23ba478de460c42d93
CarlosDiogo01 <carlos.sa01@gmail.com>
2016-03-10 11:51:53 +0000

    Organized files and close this project

---
+++ NPB3.3.1/NPB3.3-MPI/Results/Eth/gnu/32/nodo-641/CSV/get_usage_stats.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+CURRENT=`pwd`
+BASENAME=`basename "$CURRENT"`
+
+mkdir __memory_usage
+mkdir __disk_usage
+mkdir __cpu_usage
+
+
+for file in ep.C.32_filt.csv
+do
+	echo $file
+	gawk -f memory_usage.gawk $file > "MEMORY_"$file
+	gawk -f disk_usage.gawk $file > "DISK_"$file
+	gawk -f cpu_usage.gawk $file > "CPU_"$file
+done
+
+mv MEMORY_* __memory_usage
+mv DISK_* __disk_usage
+mv CPU_* __cpu_usage
---
+++ NPB3.3.1/Scripts_dstat/README.txt
@@ -0,0 +1,2 @@
+The scripts on this folder was not used in the project.
+The scripts used for DSTAT are in MPI Results folder at Eth/gnu/32/nodo-641/CSV
